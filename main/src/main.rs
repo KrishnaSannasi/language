0d61ba1b46edc533e6f639d59bd4fa33b0cd4c90
Ozaren <krishna.sd.2012@gmail.com>
2020-01-04 01:25:59 -0500

    main: added diagnostics

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,7 +1,7 @@
 use lib_thread_local::LazyThreadLocal;
 use lib_arena::local::LocalUniqueArena;
 use lib_intern::Intern;
-use core_tokens::Lexer;
+use core_tokens::{Lexer, Type};
 
 fn main() {
     let file = std::env::args().nth(1).unwrap();
@@ -15,7 +15,32 @@ fn main() {
     
     let mut lexer = impl_lexer::Lexer::new(&file, context);
 
+    let mut idents = 0;
+    let mut ints = 0;
+    let mut floats = 0;
+    let mut strs = 0;
+    let mut groups = 0;
+    let mut syms = 0;
+
     while let Some(token) = lexer.parse() {
         println!("{:?}", token);
+
+        match token.ty {
+            Type::Ident(_) => idents += 1,
+            Type::Int(_) => ints += 1,
+            Type::Float(_) => floats += 1,
+            Type::Str(_) => strs += 1,
+            Type::Grouping(_, _) => groups += 1,
+            Type::Symbol(_) => syms += 1
+        }
     }
+
+    dbg!(idents);
+    dbg!(ints);
+    dbg!(floats);
+    dbg!(strs);
+    dbg!(groups);
+    dbg!(syms);
+
+    dbg!(intern.len());
 }
