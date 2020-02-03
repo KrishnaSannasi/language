7ca7e10cf870d4949cb338439638d5fecbcdfe73
Ozaren <krishna.sd.2012@gmail.com>
2020-02-03 10:21:55 -0700

    changed over to use type based stack frame only allocate as much space as needed

--- interp-mir/src/lib.rs
+++ interp-mir/src/lib.rs
@@ -1,8 +1,15 @@
 use core_mir::{BinOpType, Load, Mir, PreOpType, Reg};
+use core_types::{Type, Primitive};
 use impl_pass_mir::encode::MirDigest;
 
+mod stack_frame;
+mod variables;
+
 pub fn interpret(digest: MirDigest) {
-    let mut mem = vec![0_i64; digest.max_reg_count];
+    let types = impl_pass_mir::type_check::infer_types(&digest).expect("Could not deduce types");
+    let mut vars = variables::Variables::new(&types);
+    
+    // let mut mem = vec![0_i64; digest.max_reg_count];
 
     let blocks = &digest.blocks;
     let mut current_block = blocks[0].as_ref().unwrap().mir.iter();
@@ -14,46 +21,87 @@ pub fn interpret(digest: MirDigest) {
         };
 
         match *mir {
-            Mir::Print(Reg(reg)) => println!("{}", mem[reg]),
+            Mir::Print(Reg(reg)) => match types[reg] {
+                Type::Primitive(Primitive::Bool) => println!("{}", vars.get::<bool>(reg)),
+                Type::Primitive(Primitive::I32) => println!("{}", vars.get::<i32>(reg)),
+                Type::Inf(_) => unreachable!(),
+            },
             Mir::Jump(target) => current_block = blocks[target].as_ref().unwrap().mir.iter(),
             Mir::BranchTrue { ref cond, target } => {
-                if mem[cond.0] != 0 {
+                if vars.get::<bool>(cond.0) {
                     current_block = blocks[target].as_ref().unwrap().mir.iter()
                 }
             }
             Mir::Load { to: Reg(to), from } => match from {
-                Load::Bool(x) => mem[to] = x as _,
-                Load::U8(x) => mem[to] = x as _,
-                Load::U16(x) => mem[to] = x as _,
-                Load::U32(x) => mem[to] = x as _,
+                Load::Bool(x) => vars.set(to, x),
+                Load::U8(x) => vars.set(to, x as i32),
+                Load::U16(x) => vars.set(to, x as i32),
+                Load::U32(_) => panic!("cannot load 32-bit literals!"),
                 Load::U64(_) => panic!("cannot load 64-bit literals!"),
                 Load::U128(_) => panic!("cannot load 128-bit literals!"),
             },
             Mir::LoadReg {
                 to: Reg(to),
                 from: Reg(from),
-            } => mem[to] = mem[from],
+            } => vars.copy(from, to),
             Mir::BinOp {
                 op,
                 out: Reg(to),
                 left: Reg(left),
                 right: Reg(right),
             } => {
-                let left = mem[left];
-                let right = mem[right];
+                match op {
+                    BinOpType::Add => {
+                        let out = vars.get::<i32>(left) + vars.get::<i32>(right);
+                        vars.set(to, out);
+                    },
+                    BinOpType::Sub => {
+                        let out = vars.get::<i32>(left) - vars.get::<i32>(right);
+                        vars.set(to, out);
+                    },
+                    BinOpType::Mul => {
+                        let out = vars.get::<i32>(left) * vars.get::<i32>(right);
+                        vars.set(to, out);
+                    },
+                    BinOpType::Div => {
+                        let out = vars.get::<i32>(left) / vars.get::<i32>(right);
+                        vars.set(to, out);
+                    },
 
-                mem[to] = match op {
-                    BinOpType::Add => left + right,
-                    BinOpType::Sub => left - right,
-                    BinOpType::Mul => left * right,
-                    BinOpType::Div => left / right,
+                    BinOpType::Equal => {
+                        let out = match types[left] {
+                            Type::Primitive(Primitive::Bool) => vars.get::<bool>(left) == vars.get(right),
+                            Type::Primitive(Primitive::I32) => vars.get::<i32>(left) == vars.get(right),
+                            Type::Inf(_) => unreachable!(),
+                        };
 
-                    BinOpType::Equal => (left == right) as _,
-                    BinOpType::NotEqual => (left != right) as _,
-                    BinOpType::LessThan => (left < right) as _,
-                    BinOpType::GreaterThan => (left > right) as _,
-                    BinOpType::LessThanOrEqual => (left <= right) as _,
-                    BinOpType::GreaterThanOrEqual => (left >= right) as _,
+                        vars.set(to, out);
+                    },
+                    BinOpType::NotEqual => {
+                        let out = match types[left] {
+                            Type::Primitive(Primitive::Bool) => vars.get::<bool>(left) != vars.get(right),
+                            Type::Primitive(Primitive::I32) => vars.get::<i32>(left) != vars.get(right),
+                            Type::Inf(_) => unreachable!(),
+                        };
+
+                        vars.set(to, out);
+                    },
+                    BinOpType::LessThan => {
+                        let out = vars.get::<i32>(left) < vars.get(right);
+                        vars.set(to, out);
+                    },
+                    BinOpType::GreaterThan => {
+                        let out = vars.get::<i32>(left) > vars.get(right);
+                        vars.set(to, out);
+                    },
+                    BinOpType::LessThanOrEqual => {
+                        let out = vars.get::<i32>(left) <= vars.get(right);
+                        vars.set(to, out);
+                    },
+                    BinOpType::GreaterThanOrEqual => {
+                        let out = vars.get::<i32>(left) >= vars.get(right);
+                        vars.set(to, out);
+                    },
                 };
             }
             Mir::PreOp {
@@ -61,12 +109,13 @@ pub fn interpret(digest: MirDigest) {
                 out: Reg(to),
                 arg: Reg(arg),
             } => {
-                let arg = mem[arg];
+                todo!();
+                // let arg = mem[arg];
 
-                mem[to] = match op {
-                    PreOpType::Not => !arg,
-                    PreOpType::Neg => -arg,
-                };
+                // mem[to] = match op {
+                //     PreOpType::Not => !arg,
+                //     PreOpType::Neg => -arg,
+                // };
             }
         }
     }
---
+++ interp-mir/src/stack_frame.rs
@@ -0,0 +1,31 @@
+use std::alloc::{alloc, dealloc, handle_alloc_error, Layout};
+
+pub struct StackFrame {
+    frame: *mut u8,
+    layout: Layout,
+}
+
+impl StackFrame {
+    pub fn new(layout: Layout) -> Self {
+        assert_ne!(layout.size(), 0);
+
+        unsafe {
+            Self {
+                frame: alloc(layout),
+                layout
+            }
+        }
+    }
+
+    pub fn ptr(&self) -> *mut u8 {
+        self.frame
+    }
+}
+
+impl Drop for StackFrame {
+    fn drop(&mut self) {
+        unsafe {
+            dealloc(self.frame, self.layout);
+        }
+    }
+}
